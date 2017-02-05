79f1f52265f52acace334c412b7f1612ec19a178
Xiao QingSong <net19880504@126.com>
2017-02-05 15:55:14 +0800

    up

--- MyBaby/app/src/main/java/com/xqs/mybaby/fragment/HomeFragment.java
+++ MyBaby/app/src/main/java/com/xqs/mybaby/fragment/HomeFragment.java
@@ -6,7 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.xqs.mybaby.R;
+import com.xqs.mybaby.adapter.CenterPagerAdapter;
 import com.xqs.mybaby.app.BaseFragment;
+import com.xqs.mybaby.ui.CenterViewPager;
+import com.xqs.mybaby.ui.ZoomOutPageTransformer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,8 +30,31 @@ public class HomeFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_first, container, false);
-        ButterKnife.bind(this.getActivity());
+        ButterKnife.bind(this,v);
+        initData();
+        initView();
         return v;
 
     }
+
+    private void initData() {
+        List<String> data=new ArrayList<String>();
+        data.add("hhh");
+        data.add("kkk");
+        data.add("kkk");
+        data.add("kkk2");
+        data.add("kkk3");
+        data.add("kkk23");
+        data.add("kkk31");
+        CenterPagerAdapter adapter=new CenterPagerAdapter(this.app,data);
+        mViewPager.setAdapter(adapter);
+//        mViewPager.enableCenterLockOfChilds();
+        mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+    }
+
+    private void initView() {
+
+    }
+
+
 }
